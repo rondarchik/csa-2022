fafb369b587e5fd4f13c934d2376f94ebe0d12b5
rondarchik <dovgun-viktoriya@mail.ru>
2022-05-30 03:25:50 +0300

    lab3 done ╰(▔∀▔)╯

--- lab03/ex4.s
+++ lab03/ex4.s
@@ -1,9 +1,11 @@
+# 10 - Довгун Виктория Александровна	1
+# Реализуйте функцию вычисления факториала. Эта функция принимает один целочисленный параметр n и возвращает n!.
+
 .globl iterative
 .globl recursive
 
 .data
-n: .word 8
-m: .word 2
+n: .word 5
 
 .text
 main:
@@ -23,14 +25,58 @@ main:
     ecall # Exit
 
 tester:
-    # YOUR CODE HERE
-    # вызвать итеративную и рекурсивные функции, сравнить ответ и вернуть результат, если совпал. иначе вернуть -1.
-	jalr zero, 0(ra)
+    addi sp, sp, -8
+    sw x10, 4(sp)
+    sw x1, 0(sp)
+
+	jal x1, iterative
+	addi x7, t0, 0
+
+	jal x1, recursive
+
+	beq x7, x10, finish_test
+	addi a0, x0, 10
+	ecall
+
+	finish_test:
+        lw ra, 0(sp)
+        addi sp, sp, 8
+        jr ra
 
 iterative:
-    # YOUR CODE HERE
-	jalr zero, 0(ra)
+    addi t0, zero, 1 # temp = 1
+    blt a0, t1, else # если n < 1
+
+    li x6, 1 # int i = 1
+    mv x5, a0
+    loop:
+        mul x5, x6, x5 # a0 = a0*i
+        addi x6, x6, 1 # i++
+        blt x6, a0, loop # if (i <= N)
+
+	jr ra
     
 recursive:
-    # YOUR CODE HERE
-	jalr zero, 0(ra)
+    addi sp, sp, -8
+    sw x1, 4(sp)
+    sw x10, 0(sp)
+
+    addi x5, x10, -1 
+    bgt x5, x0, else 
+
+    addi x10, zero, 1
+    addi sp, sp, 8
+    jr x1
+
+else:
+    addi x10, x10, -1
+    jal x1, recursive
+    addi x6, x10, 0
+    lw x10, 0(sp)
+    lw x1, 4(sp)
+    addi sp, sp, 8
+    mul x10, x10, x6
+
+    jr x1
+
+
